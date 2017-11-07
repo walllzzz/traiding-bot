14adf73ab90f101410c8988904c77160388a3c2d
walid <noutest@gmail.com>
2017-11-07 09:01:09 +0100

    delete key

--- src/main/java/org/knowm/xchange/examples/kraken/KrakenExampleUtils.java
+++ src/main/java/org/knowm/xchange/examples/kraken/KrakenExampleUtils.java
@@ -13,8 +13,8 @@ public class KrakenExampleUtils {
   public static Exchange createTestExchange() {
 
     Exchange krakenExchange = ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class.getName());
-    krakenExchange.getExchangeSpecification().setApiKey("0L0xzNc3/4vKSPJu19CLRsu6HHZ1e1ppFyXB0j+CSx+RkRSpAz07dmwy");
-    krakenExchange.getExchangeSpecification().setSecretKey("8jSPJQGZN1yuj9RnePbJErRI94gtHGrHTs5rBVQtjSlIrYBL+rFzrlsBGxv3u630FDdZpWfVxyQr8MLU5KhHpg==");
+    krakenExchange.getExchangeSpecification().setApiKey("");
+    krakenExchange.getExchangeSpecification().setSecretKey("");
     krakenExchange.getExchangeSpecification().setUserName("walllzzz");
     krakenExchange.applySpecification(krakenExchange.getExchangeSpecification());
     return krakenExchange;
